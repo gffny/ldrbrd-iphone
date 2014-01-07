f9076e51fc58c57c072aee33985cbd0d1549c1e3
gffny <gaffney.ie@gmail.com>
2014-01-07 13:59:37 -0500

    initalising repo for ldbrd-iphone

---
+++ ldrbrd-iphone/lbAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  lbAppDelegate.h
+//  ldrbrd-iphone
+//
+//  Created by John D. Gaffney on 1/7/14.
+//  Copyright (c) 2014 gffny.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface lbAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
